49a163db3816a20126ccc7d0bb1b3c3565407c26
kesadillas <126735859+alekesadillas@users.noreply.github.com>
2025-04-18 11:34:46 -0600

    db.py

--- src/database/db.py
+++ src/database/db.py
@@ -1,6 +1,5 @@
 import psycopg2
-from psycopg2 import DatabaseError
-from decouple import config
+
 
 def get_connection():
     try:
@@ -8,8 +7,7 @@ def get_connection():
             host=config('PGSQL_HOST'),
             port=config('PGSQL_PORT'),
             user=config('PGSQL_USER'),
-            password=config('PGSQL_PASSWORD'),
-            database=config('PGSQL_DATABASE')
+
 
         )
     except DatabaseError as ex:
